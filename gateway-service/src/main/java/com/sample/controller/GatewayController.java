1d8bf0d2285b430720f8f3c5d71e4c5cf6c09765
Carlos Requena <requenacj@gmail.com>
2019-07-09 16:03:00 +0200

    misc

--- gateway-service/src/main/java/com/sample/controller/GatewayController.java
+++ gateway-service/src/main/java/com/sample/controller/GatewayController.java
@@ -1,20 +1,40 @@
 package com.sample.controller;
 
 import com.sample.service.GatewayService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+
+@Log4j2
 @RestController
 public class GatewayController {
+
+  private GatewayService gateway;
+  private final ExecutorService pool;
+
   @Autowired
-  GatewayService gateway;
+  public GatewayController(GatewayService gateway){
+    this.gateway = gateway;
+    this.pool = new ForkJoinPool(1000);
+  }
 
   @GetMapping(value = "/string/{string}")
-  public ResponseEntity<String> getUser(@PathVariable("string") String string) {
-    return new ResponseEntity<String>(gateway.process(string), HttpStatus.OK);
+  public DeferredResult<ResponseEntity<?>> process(@PathVariable("string") String string) {
+    DeferredResult<ResponseEntity<?>> deferredResult = new DeferredResult<>(500l);
+    String operationIdentifier = java.util.UUID.randomUUID().toString();
+    pool.submit(() -> {
+      deferredResult.setResult(ResponseEntity.ok(this.gateway.process(string)));
+    });
+    return deferredResult;
   }
 }
