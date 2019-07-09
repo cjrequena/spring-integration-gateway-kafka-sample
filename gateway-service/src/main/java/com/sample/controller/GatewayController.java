4c42deceef2d003587a1dc6a8e856d7050a9e35a
Carlos Requena <requenacj@gmail.com>
2019-07-09 16:08:48 +0200

    misc

--- gateway-service/src/main/java/com/sample/controller/GatewayController.java
+++ gateway-service/src/main/java/com/sample/controller/GatewayController.java
@@ -3,15 +3,12 @@ package com.sample.controller;
 import com.sample.service.GatewayService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinPool;
 
