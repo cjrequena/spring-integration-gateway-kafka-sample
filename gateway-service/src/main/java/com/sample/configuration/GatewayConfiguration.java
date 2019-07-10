d9439855e4366ee3a165ffa7b6e1e383d95caaa6
Carlos Requena <requenacj@gmail.com>
2019-07-10 16:54:02 +0200

    misc

--- gateway-service/src/main/java/com/sample/configuration/GatewayConfiguration.java
+++ gateway-service/src/main/java/com/sample/configuration/GatewayConfiguration.java
@@ -1,6 +1,10 @@
 package com.sample.configuration;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sample.channel.KafkaChannel;
+import com.sample.dto.FooDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -11,20 +15,40 @@ import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.messaging.Message;
 
+import java.io.IOException;
+import java.util.UUID;
+
 @Configuration
 @EnableIntegration
 public class GatewayConfiguration {
 
-  //public static final UUID instanceUUID = UUID.randomUUID();
-  public static final String instanceUUID = "GATEWAY_1";
+  public static final UUID instanceUUID = UUID.randomUUID();
+
+  @Autowired
+  private ObjectMapper objectMapper;
 
   @Bean
   @IntegrationConverter
   public Converter<byte[], String> bytesToStringConverter() {
     return new Converter<byte[], String>() {
       @Override
-      public String convert(byte[] source) {
-        return new String(source);
+      public String convert(byte[] message) {
+        return new String(message);
+      }
+    };
+  }
+
+  @Bean
+  @IntegrationConverter
+  public Converter<byte[], FooDTO> bytesToFooDTOConverter() {
+    return new Converter<byte[], FooDTO>() {
+      @Override
+      public FooDTO convert(byte[] message) {
+        try {
+          return objectMapper.readValue(message, new TypeReference<FooDTO>() {});
+        } catch (IOException e) {
+          return null;
+        }
       }
     };
   }
---
+++ gateway-service/src/main/java/com/sample/dto/FooDTO.java
@@ -0,0 +1,23 @@
+package com.sample.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * @author cjrequena
+ * @version 1.0
+ * @since JDK1.8
+ * @see
+ *
+ */
+@Data
+public class FooDTO implements Serializable{
+  private String id;
+  private String name;
+
+}
